742f7f9b1bc8d310e2894cf018134a44080a03f3
dangcao <dangcao410@gmail.com>
2021-03-05 14:36:01 +0700

    Update.

---
+++ pages/settings.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="page">
+    <div class="nav">
+      <ul>
+        <li>
+          <nuxt-link to="/settings">
+            General
+          </nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/settings/deck">
+            Deck
+          </nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/settings/schedule">
+            Schedule
+          </nuxt-link>
+        </li>
+      </ul>
+    </div>
+    <nuxt-child />
+  </div>
+</template>
+
+<style scoped>
+  .page {
+    display: flex;
+  }
+</style>
---
+++ pages/settings/deck/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <main class="main">
+    <h3>Deck settings.</h3>
+  </main>
+</template>
+
+<style scoped>
+  .main {
+    margin-left: 3rem;
+  }
+</style>
