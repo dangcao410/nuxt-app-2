2407f1d09d7d38f35713549de36be03c7f3a4482
dangcao <dangcao410@gmail.com>
2021-03-05 11:37:10 +0700

    Update.

---
+++ pages/decks/_id/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Deck {{ $route.params.id }}</h1>
+</template>
---
+++ pages/decks/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Decks, enter number of deck to show</h1>
+    <input v-model="deckId" type="text" placeholder="Enter deckID">
+    <button @click="showDeck">
+      Go to Deck
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      deckId: '1'
+    }
+  },
+  methods: {
+    showDeck() {
+      this.$router.push(`/decks/${this.deckId}`)
+    }
+  }
+}
+</script>
