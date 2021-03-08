438fcbc94b00740fe0c2ffdee279d90e96e3fc78
dangcao <dangcao410@gmail.com>
2021-03-08 09:25:04 +0700

    Update.

--- pages/decks/index.vue
+++ pages/decks/index.vue
@@ -4,7 +4,7 @@
 
     <v-row>
       <v-col cols="12" sm="6">
-        <v-text-field v-model="deckId" label="Enter Deck ID" width="30%"></v-text-field>
+        <v-text-field v-model="deckId" label="Enter Deck ID"></v-text-field>
       </v-col>
       <v-col>
         <v-btn @click="showDeck">Go to Deck</v-btn>
